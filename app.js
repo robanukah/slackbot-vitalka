8f6192b8e310942d4a68e61c4fe96446c045741c
solairerove <hax2033@gmail.com>
2017-01-16 12:01:16 +0300

    Add simple vk api implementation

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
+'user strict';
+
 var Botkit = require('botkit');
+var VKApi = require('node-vkapi');
 
 var controller = Botkit.slackbot({
     debug: false
 });
 
+var vk = new VKApi();
+
 controller.spawn({
     token: process.env.token || ''
-}).startRTM(function (err) {
+}).startRTM(function(err) {
     if (err) {
         throw new Error(err);
     }
 });
 
 controller.hears(['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
-    function (bot, message) {
+    function(bot, message) {
         bot.reply(message, 'Hello yourself.');
     }
 );
 
-controller.on('direct_mention', function (bot, message) {
+controller.on('direct_mention', function(bot, message) {
     bot.reply(message, 'I heard you mention me!');
 });
 
-controller.hears(['random'], ['direct_mention'], function (bot, message) {
-    bot.say(
-        {
-            text: 'Message to random channel',
+controller.hears(['random'], ['direct_mention'], function(bot, message) {
+    bot.say({
+        text: 'Message to random channel',
+        channel: '#random'
+    });
+});
+
+controller.hears(['durov'], ['direct_message'], function(bot, message) {
+    var response = new String();
+
+    vk.call('users.get', {
+        user_ids: ['1', '2']
+    }).then(res => {
+        bot.say({
+            text: 'id: ' + res[0].id + '\n'
+            'first_name: ' + res[0].first_name + '\n' +
+            'last_name: ' + res[0].last_name + '\n' + ,
             channel: '#random'
-        }
-    );
+        });
+        console.log(res);
+    })
 });
