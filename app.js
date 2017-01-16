5e06542875845eed368c819fab78caf86a46ae0e
solairerove <hax2033@gmail.com>
2017-01-16 15:44:15 +0300

    Simplify app.js

--- app.js
+++ app.js
@@ -4,20 +4,13 @@ var Botkit = require('botkit');
 var VKApi = require('node-vkapi');
 
 var LEPRO_ID = -65960786;
+var LEPRO_WALL = 'lepro';
+var VK_API_VERSION = 5.62;
+
+var vk = new VKApi();
 
 var controller = Botkit.slackbot({
     debug: false
-});
-
-var vk = new VKApi({
-    app: {
-        id: '<app_id>',
-        secret: 'app_secret'
-    },
-    auth: {
-        login: 'login',
-        pass: 'pass'
-    }
 });
 
 controller.spawn({
@@ -28,22 +21,15 @@ controller.spawn({
     }
 });
 
-controller.hears(['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
-    function(bot, message) {
-        bot.reply(message, 'Hello yourself.');
-    }
-);
-
-controller.hears(['lepro'], ['direct_mention'], function(bot, message) {
-    vk.auth.user().then(token => {
-        return vk.call('wall.get', {
-            owner_id: LEPRO_ID,
-            version: 5.62
-        }).then(res => {
-            bot.reply(message, res.items[0].attachments[0].photo.photo_604);
-            console.log(res);
-        }).catch(error => {
-            console.log(error);
-        });
+controller.hears([LEPRO_WALL], ['direct_mention'], function(bot, message) {
+    vk.call('wall.get', {
+        owner_id: LEPRO_ID,
+        version: VK_API_VERSION
+    }).then(res => {
+        bot.reply(message, res.items[0].attachments[0].photo.photo_604);
+        console.log(res);
+    }).catch(error => {
+        console.log(error);
+        bot.reply(message, res.items[2].attachments[0].photo.photo_604);
     });
 });
